596faae98ccaba63a5ecce88a050df16e4cf0306
MitsuyoshiToguri <seiun340066@gmail.com>
2014-11-04 16:24:14 -0500

    I displayed pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,20 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = [
+			{ 
+				:title => "The old church on the coast white sea",
+				:artist => "Sergey Ershv",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+			},
+			{  	:title => "Sea Power",
+				:artist => "Sthepen Scullion",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+			},
+
+			{ 	:title => "Intro the Poppies",
+				:artist => "Jhon Whilhelm",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+			}
+		]
 	end
 end
