2ef3564eb58ac407e521def566e3efe808a73b88
MitsuyoshiToguri <seiun340066@gmail.com>
2014-11-04 17:08:06 -0500

    Making each pictuire show at its own URL

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -17,4 +17,27 @@ class PicturesController < ApplicationController
 			}
 		]
 	end
+
+
+def show
+		@pictures = [
+			{ 
+				:title => "The old church on the coast white sea",
+				:artist => "Sergey Ershv",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+			},
+			{  	:title => "Sea Power",
+				:artist => "Sthepen Scullion",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+			},
+
+			{ 	:title => "Intro the Poppies",
+				:artist => "Jhon Whilhelm",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+			}
+		]
+
+		@picture = @pictures[params[:id].to_i]
+	end
+
 end
